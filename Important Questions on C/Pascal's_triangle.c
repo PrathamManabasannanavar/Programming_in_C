391d42f37879092a9b651880fb3a0bcebf264b24
PrathamManabasannanavar <128047712+PrathamManabasannanavar@users.noreply.github.com>
2023-04-27 12:15:27 +0530

    Add files via upload

---
+++ Important Questions on C/Pascal's_triangle.c
@@ -0,0 +1,36 @@
+/*A simple C program to compute and 
+print the Pascal's triangle */
+
+#include<stdio.h>
+
+main()
+
+{
+    int rows, rem, val=1, j, i;
+    
+    printf("Enter no of rows\n");
+    scanf("%d",&rows);
+    
+    int len=rows;
+    
+    for(i=0; i<rows; i++)
+    {
+       for(j=0; j<len; j++) //for giving space
+       {
+           printf(" ");
+       }
+       len--;
+       
+       val*=11;
+       int var=val;
+       
+      while(var!=0)
+      {
+      rem=var%10;
+      printf("%d ",rem);
+      var=var/10;
+      }
+      printf("\n");
+    }
+    
+}
